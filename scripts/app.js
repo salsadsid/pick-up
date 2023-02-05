d28ddb24c7ab9346dee53dafb56f1a733ff14e63
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-02-05 19:48:47 +0600

    contact-form

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,6 @@ const resNav=document.querySelector("ul")
 menu.addEventListener("click", () =>{
     resNav.classList.toggle('open')
 });
+const year = document.getElementById("year");
+
+year.textContent = new Date().getFullYear();
